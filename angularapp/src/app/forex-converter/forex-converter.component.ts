9e8aff5e0c7e6d6f9dcf7238adafc3741a042f2f
Iamneo <techadmin@iamneo.ai>
2023-09-06 15:28:32 +0000

    Backup Commit

--- angularapp/src/app/forex-converter/forex-converter.component.ts
+++ angularapp/src/app/forex-converter/forex-converter.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./forex-converter.component.css']
 })
 export class ForexConverterComponent {
+  exchangeRates = {
+    USD: 1.126735,
+    GBP: 0.876893,
+    INR: 79.677056
+  };
 
+  convertedAmount: number | null = null;
+
+  convertCurrency(formData: any): void {
+    const fromCurrency = formData.fromCurrency;
+    const toCurrency = formData.toCurrency;
+    const amount = parseFloat(formData.amount);
+
+    if (isNaN(amount)) {
+      this.convertedAmount = null;
+      return;
+    }
+
+    if (fromCurrency === toCurrency) {
+      this.convertedAmount = amount;
+    } else {
+      const conversionRate = this.exchangeRates[fromCurrency] / this.exchangeRates[toCurrency];
+      this.convertedAmount = amount * conversionRate;
+    }
+  }
 }
