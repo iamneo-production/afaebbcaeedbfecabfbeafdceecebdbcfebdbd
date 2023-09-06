c962f62383bad57f5a875cd4c6cdc8efb03b3197
Iamneo <techadmin@iamneo.ai>
2023-09-06 15:33:31 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,28 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CurrencyconverterComponent } from './currencyconverter/currencyconverter.component';
-import { CurrencyformComponent } from './currencyform/currencyform.component';
-import { ResultComponent } from './result/result.component';
-import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
-import { CurrencyFormComponent } from './currency-form/currency-form.component';
-import { CurrencyResultComponent } from './currency-result/currency-result.component';
 import { ForexConverterComponent } from './forex-converter/forex-converter.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrencyconverterComponent,
-    CurrencyformComponent,
-    ResultComponent,
-    CurrencyConverterComponent,
-    CurrencyFormComponent,
-    CurrencyResultComponent,
     ForexConverterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
