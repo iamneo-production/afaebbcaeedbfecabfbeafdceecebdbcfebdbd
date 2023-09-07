b6ca1b307d757c4f3e41063d855201df318f34fd
Iamneo <techadmin@iamneo.ai>
2023-09-07 14:24:26 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ForexService } from '../forex.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-forex-converter',
+  templateUrl: './forex-converter.component.html',
+  styleUrls: ['./forex-converter.component.css']
 })
-export class AppComponent {
-  title = 'angularapp';
+export class ForexConverterComponent {
+  convertedAmount: number | undefined;
+  resultMessage: string | undefined;
+
+  constructor(private forexService: ForexService) { }
+
+  convertCurrency(form: any) {
+    const fromCurrency = form.value.fromCurrency;
+    const toCurrency = form.value.toCurrency;
+    const amount = form.value.amount;
+
+    const exchangeRate = this.forexService.getExchangeRate(fromCurrency, toCurrency);
+
+    if (!isNaN(amount)) {
+      this.convertedAmount = amount * exchangeRate;
+      this.resultMessage = `${amount} ${fromCurrency} = ${this.convertedAmount.toFixed(2)} ${toCurrency}`;
+    } else {
+      this.resultMessage = "Please enter a valid amount.";
+    }
+  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForexConverterComponent } from './forex-converter/forex-converter.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
