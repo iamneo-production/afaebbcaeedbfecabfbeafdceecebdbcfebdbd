5fa7699f4b5fa0a3da36bad81d32c47e467fa00c
Iamneo <techadmin@iamneo.ai>
2023-09-07 14:14:26 +0000

    Backup Commit

---
+++ angularapp/src/app/forex-converter/forex-converter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forex-converter',
+  templateUrl: './forex-converter.component.html',
+  styleUrls: ['./forex-converter.component.css']
+})
+export class ForexConverterComponent {
+
+}
